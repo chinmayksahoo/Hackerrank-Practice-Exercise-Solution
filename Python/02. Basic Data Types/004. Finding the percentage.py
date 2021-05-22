8ef33b1ff796f18ada5e89a2e179c81993c65364
Chinmay <chinmaya228@gmail.com>
2021-05-22 23:30:48 +0530

    Updated Finding the Percentage Solution

--- Python/02. Basic Data Types/004. Finding the percentage.py
+++ Python/02. Basic Data Types/004. Finding the percentage.py
@@ -14,3 +14,17 @@ listOfStudents = dict()
 for i in range(n):
     readScores(listOfStudents)
 print('%.2f' % listOfStudents[input()])
+
+# Alternative Solution
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     student_marks = {}
+    
+#     for _ in range(n):
+#         name, *line = input().split()
+#         scores = list(map(float, line))
+#         student_marks[name] = scores
+#     query_name = input()
+#     query_scores = student_marks[query_name]
+#     print("{0:.2f}".format(sum(query_scores)/len(query_scores)))
