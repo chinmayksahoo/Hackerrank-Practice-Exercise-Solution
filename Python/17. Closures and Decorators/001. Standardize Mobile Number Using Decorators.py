5de051309a520a92b68020208f12ca617b1a87bb
Chinmay <chinmaya228@gmail.com>
2021-05-22 23:51:31 +0530

    Added Closures and Decorators

---
+++ Python/17. Closures and Decorators/001. Standardize Mobile Number Using Decorators.py
@@ -0,0 +1,17 @@
+# Problem: https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators/problem
+# Score: 30
+
+def wrapper(f):
+    def fun(l):
+        # complete the function
+        return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
+
+
---
+++ Python/17. Closures and Decorators/002. Decorators 2 - Name Directory.py
@@ -0,0 +1,17 @@
+# Problem: https://www.hackerrank.com/challenges/decorators-2-name-directory/problem
+# Score: 30
+
+import operator
+
+def person_lister(f):
+    def inner(people):
+        # complete the function
+        return inner
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
